9bd43060fa4103ca845f944862d1a365f248461b
gargriya01 <72300799+gargriya01@users.noreply.github.com>
2023-08-14 12:34:24 +0530

    DONE TILL"GULP"

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const dotenv = require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const env = require("./config/environment");
+const logger = require("morgan");
+
 const cookieParser = require("cookie-parser");
 const app = express();
+
+require("./config/view-helpers")(app);
+
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
@@ -40,6 +45,9 @@ app.use(express.static(env.asset_path));
 
 // make the uploads path available to the browser
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
+// morgan
+app.use(logger(env.morgan.mode, env.morgan.options));
 
 app.use(expressLayouts);
 
